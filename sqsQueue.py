00f5113b9706b82595b2f53300f09efae8cb9992
OliviaZ <lz2933@columbia.edu>
2023-11-23 19:58:28 +0000

    resolve safety issue

--- sqsQueue.py
+++ sqsQueue.py
@@ -6,12 +6,10 @@ import boto3
 # Create the queue. 
 sqs_client = boto3.client(
   "sqs", 
-  region_name="us-east-1", 
-  aws_access_key_id='AKIA3XW3AFUMDMXLXCU4',
-  aws_secret_access_key='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')
+  region_name="us-east-1")
 
 response = sqs_client.create_queue(
-    QueueName="test_gcp",
+    QueueName="test_new_credential",
     Attributes={
         "DelaySeconds": "0",
         "VisibilityTimeout": "60",  # 60 seconds
